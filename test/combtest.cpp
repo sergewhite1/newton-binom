ab15c806d5b65a3a03557d24cf1f005208e38344
Sergey Belov <serge_belov@ukr.net>
2020-08-10 18:37:59 +0300

    Added new unit tests.

--- test/combtest.cpp
+++ test/combtest.cpp
@@ -13,6 +13,15 @@ int TestCase1() {
 }
 
 int TestCase2() {
+  uint64_t c = comb(19, 17);
+  if (c == 171) {
+    return 0;
+  }
+  return 1;
+}
+
+
+int TestCase3() {
   uint64_t c = comb(0, 0);
   if (c == 1) {
     return 0;
@@ -20,8 +29,23 @@ int TestCase2() {
   return 1;
 }
 
+int TestCase4() {
+  uint64_t c = comb(5, 0);
+  if (c == 1) {
+    return 0;
+  }
+  return 1;
+}
 
-static const TestCaseProc TestCases[] = {nullptr, TestCase1, TestCase2};
+int TestCase5() {
+  uint64_t c = comb(5, 5);
+  if (c == 1) {
+    return 0;
+  }
+  return 1;
+}
+static const TestCaseProc TestCases[] = {nullptr, TestCase1, TestCase2, 
+  TestCase3, TestCase4, TestCase5};
 
 
 int main(int argc, char* argv[]) {
