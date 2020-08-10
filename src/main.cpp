7f565b99e15f281e1f6aee2fbc830c879e75236c
Sergey Belov <serge_belov@ukr.net>
2020-08-10 20:29:54 +0300

    In progress.

--- src/main.cpp
+++ src/main.cpp
@@ -18,25 +18,25 @@ int main() {
 
   int a = 3;
   int b = 2;
-  int n = 10;
+  int n = 9;
 
   std::cout << " a = " << a 
             << " b = " << b
             << " n = " << n << std::endl;
 
-  int A1 = powi(a + b, n);
-  std::cout << "A1 = powi(a + b, n) = " << A1 << std::endl;
+  int A1 = powi(a + b, n + 1);
+  std::cout << "A1 = powi(a + b, n + 1) = " << A1 << std::endl;
 
   int B1 = 0;
-  for (int m = 0; m <= n; ++m) {
-    B1 += comb(n,m) * powi(a, n - m) * powi(b, m);
+  for (int m = 0; m <= n + 1; ++m) {
+    B1 += comb(n + 1, m) * powi(a, n + 1 - m) * powi(b, m);
   }
   std::cout << "B1 = " << B1 << std::endl;
 
   int B2 = 0;
-  for (int m = 0; m <= n - 1; ++m) {
-    B2 += comb(n - 1, m) * powi(a, n - m)     * powi(b, m);
-    B2 += comb(n - 1, m) * powi(a, n - 1 + m) * powi(b, m + 1);
+  for (int m = 0; m <= n; ++m) {
+    B2 += comb(n, m) * powi(a, n + 1 - m) * powi(b, m);
+    B2 += comb(n, m) * powi(a, n - m)     * powi(b, m + 1);
   }
   std::cout << "B2 = " << B2 << std::endl;
   
